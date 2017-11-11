439c69501ceedd42eb82fbf960b4b1080d0658c3
miup <contact@miup.blue>
2017-11-11 23:19:34 +0900

    fix

--- Algent/Classes/Agent.swift
+++ Algent/Classes/Agent.swift
@@ -11,7 +11,7 @@ import Result
 public typealias AlgentQuery = Query
 
 public class Algent {
-    public private(set) static var shared: Agent!
+    public private(set) static var shared: Algent!
 
     private let client = Client(appID: Algent.appID, apiKey: Algent.apiKey)
     private static var appID: String!
@@ -35,7 +35,7 @@ public class Algent {
         let index = client.index(withName: request.indexName)
         index.search(request.query) { (json, error) in
             if let error = error { print(error); return }
-            guard let json: JSONObject = json else { return }
+            guard let json: [String: Any] = json else { return }
             do {
                 let data = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
                 print(String(data: data, encoding: .utf8)!)
@@ -52,7 +52,7 @@ public class Algent {
         if let query = request.query {
             index.z_objc_searchForFacetValues(of: request.key, matching: request.keyword, query: query) { (json, error) in
                 if let error = error { print(error); return }
-                guard let json: JSONObject = json else { return }
+                guard let json: [String: Any] = json else { return }
                 do {
                     let data = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
                     print(String(data: data, encoding: .utf8)!)
@@ -65,7 +65,7 @@ public class Algent {
         } else {
             index.searchForFacetValues(of: request.key, matching: request.keyword) { (json, error) in
                 if let error = error { print(error); return }
-                guard let json: JSONObject = json else { return }
+                guard let json: [String: Any] = json else { return }
                 do {
                     let data = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
                     print(String(data: data, encoding: .utf8)!)
--- Algent/Classes/Request.swift
+++ Algent/Classes/Request.swift
@@ -9,12 +9,12 @@ import AlgoliaSearch
 
 public protocol AlgoliaRequestProtocol {
     associatedtype HitType: Decodable
-    var query: AgentQuery { get }
+    var query: AlgentQuery { get }
     var indexName: String { get }
 }
 
 public protocol FacetValuesRequestProtocol {
-    var query: AgentQuery? { get }
+    var query: AlgentQuery? { get }
     var indexName: String { get }
     var key: String { get }
     var keyword: String { get }
