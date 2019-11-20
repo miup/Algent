2407394fc6dbcd7a21c97690977b1fe278346896
sgr-ksmt <suguru-kishimoto@cookpad.com>
2019-11-20 15:21:15 +0900

    Support Carthage.

---
+++ Algent/Algent.h
@@ -0,0 +1,18 @@
+//
+//  Algent.h
+//  Algent
+//
+//  Created by suguru-kishimoto on 2019/11/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Algent.
+FOUNDATION_EXPORT double AlgentVersionNumber;
+
+//! Project version string for Algent.
+FOUNDATION_EXPORT const unsigned char AlgentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Algent/PublicHeader.h>
+
+
